3658b3c134057a110c8037d69687d71c392c8bdc
fullyalive <wogud1202@gmail.com>
2018-05-10 08:41:26 +0900

    initial setup

---
+++ styled-awesomeness/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <button className="button">
+       Hello!
+      </button>
+    );
+  }
+}
+
+export default App;
